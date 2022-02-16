8df075d157931501feb5fe80bfccbaa986f55369
mattis - Helife <mattis.dalleau@epitech.eu>
2022-02-16 11:38:32 +0400

    Fix gates

--- src/gates/Gates.cpp
+++ src/gates/Gates.cpp
@@ -11,13 +11,12 @@ Tristate andGate(const Tristate a, const Tristate b) {
 }
 
 Tristate nandGate(const Tristate a, const Tristate b) {
-    const Tristate res = andGate(a, b);
-    if (res == TRUE)
-        return FALSE;
-    else if (res == FALSE)
-        return TRUE;
-    else
+    if (a == UNDEFINED || b == UNDEFINED)
         return UNDEFINED;
+    else if (a == TRUE && b == TRUE)
+        return FALSE;
+    else
+        return TRUE;
 }
 
 Tristate xorGate(const Tristate a, const Tristate b) {
@@ -27,15 +26,14 @@ Tristate xorGate(const Tristate a, const Tristate b) {
         return TRUE;
     else if (a == FALSE && b == TRUE)
         return TRUE;
-    else if (a == TRUE && b == TRUE)
-        return FALSE;
     else
         return FALSE;
 }
 
 Tristate orGate(const Tristate a, const Tristate b) {
-    if (a == UNDEFINED || b == UNDEFINED) return UNDEFINED;
-    if (a == TRUE || b == TRUE)
+    if (a == UNDEFINED || b == UNDEFINED)
+        return UNDEFINED;
+    else if (a == TRUE || b == TRUE)
         return TRUE;
     else
         return FALSE;
