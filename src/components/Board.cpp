42c7aa13c13e62163dcf365f903b0eb2b4cb6075
Alexis Cheron <git@norech.com>
2022-03-03 10:06:51 +0400

    removed in_ and out_ and using pin_

--- src/components/Board.cpp
+++ src/components/Board.cpp
@@ -1,17 +1,26 @@
 #include "Board.hpp"
+
+#include "SpecialComponent.hpp"
+
 namespace nts {
+
 Board::Board(Circuit* circuit)
     : Component(circuit->getName()),
       _circuit(std::unique_ptr<Circuit>(circuit)) {
     for (auto& component : circuit->getComponents()) {
-        if (component.first.find("in_") == 0) {
-            addInputPin(std::atoi(component.first.substr(3).c_str()));
-        }
-        else if (component.first.find("out_") == 0) {
-            addOutputPin(std::atoi(component.first.substr(4).c_str()));
+        if (component.first.find("pin_") == 0) {
+            int pinId = std::stoi(component.first.substr(4));
+
+            if (dynamic_cast<Input*>(component.second.get())) {
+                addInputPin(pinId);
+            }
+            else if (dynamic_cast<Output*>(component.second.get())) {
+                addOutputPin(pinId);
+            }
         }
     }
 }
+
 void Board::simulate() { _circuit->simulate(); }
 
 Tristate Board::compute(std::size_t pinIndex) {
@@ -19,19 +28,19 @@ Tristate Board::compute(std::size_t pinIndex) {
         for (auto& it : _pins) {
             Pin& pin = *it.second;
             if (pin.getType() == INPUT) {
-                _circuit->setInput("in_" + std::to_string(pin.getPin()),
+                _circuit->setInput("pin_" + std::to_string(pin.getPin()),
                                    pin.compute());
             }
         }
         _circuit->simulate();
-        return _circuit->getOutput("out_" + std::to_string(pinIndex));
+        return _circuit->getOutput("pin_" + std::to_string(pinIndex));
     }
     else {
         Pin& pin = getPin(pinIndex);
         Tristate state = pin.compute();
-        _circuit->setInput("in_" + std::to_string(pinIndex), state);
+        _circuit->setInput("pin_" + std::to_string(pinIndex), state);
         return state;
     }
-}  // namespace nts
+}
 
 }  // namespace nts
--- src/components/ComponentFactory.cpp
+++ src/components/ComponentFactory.cpp
@@ -8,21 +8,21 @@ namespace nts {
 Board *ComponentFactory::do4071() {
     Circuit *circuit = new Circuit("4071");
 
-    circuit->addComponent("input", "in_1", "or1_a");
-    circuit->addComponent("input", "in_2", "or1_b");
-    circuit->addComponent("output", "out_3", "or1_s");
+    circuit->addComponent("input", "pin_1", "or1_a");
+    circuit->addComponent("input", "pin_2", "or1_b");
+    circuit->addComponent("output", "pin_3", "or1_s");
 
-    circuit->addComponent("output", "out_4", "or2_a");
-    circuit->addComponent("input", "in_5", "or2_b");
-    circuit->addComponent("input", "in_6", "or2_s");
+    circuit->addComponent("output", "pin_4", "or2_a");
+    circuit->addComponent("input", "pin_5", "or2_b");
+    circuit->addComponent("input", "pin_6", "or2_s");
 
-    circuit->addComponent("input", "in_8", "or3_a");
-    circuit->addComponent("input", "in_9", "or3_b");
-    circuit->addComponent("output", "out_10", "or3_s");
+    circuit->addComponent("input", "pin_8", "or3_a");
+    circuit->addComponent("input", "pin_9", "or3_b");
+    circuit->addComponent("output", "pin_10", "or3_s");
 
-    circuit->addComponent("output", "out_11", "or4_a");
-    circuit->addComponent("input", "in_12", "or4_b");
-    circuit->addComponent("input", "in_13", "or4_s");
+    circuit->addComponent("output", "pin_11", "or4_a");
+    circuit->addComponent("input", "pin_12", "or4_b");
+    circuit->addComponent("input", "pin_13", "or4_s");
 
     circuit->addComponent("_or", "or1");
     circuit->addComponent("_or", "or2");
@@ -51,23 +51,23 @@ Board *ComponentFactory::do4071() {
 Board *ComponentFactory::do4069() {
     Circuit *circuit = new Circuit("4069");
 
-    circuit->addComponent("input", "in_1", "not1_i");
-    circuit->addComponent("output", "out_2", "not1_o");
+    circuit->addComponent("input", "pin_1", "not1_i");
+    circuit->addComponent("output", "pin_2", "not1_o");
 
-    circuit->addComponent("input", "in_3", "not2_i");
-    circuit->addComponent("output", "out_4", "not2_o");
+    circuit->addComponent("input", "pin_3", "not2_i");
+    circuit->addComponent("output", "pin_4", "not2_o");
 
-    circuit->addComponent("input", "in_5", "not3_i");
-    circuit->addComponent("output", "out_6", "not3_o");
+    circuit->addComponent("input", "pin_5", "not3_i");
+    circuit->addComponent("output", "pin_6", "not3_o");
 
-    circuit->addComponent("input", "in_9", "not4_i");
-    circuit->addComponent("output", "out_8", "not4_o");
+    circuit->addComponent("input", "pin_9", "not4_i");
+    circuit->addComponent("output", "pin_8", "not4_o");
 
-    circuit->addComponent("input", "in_11", "not5_i");
-    circuit->addComponent("output", "out_10", "not5_o");
+    circuit->addComponent("input", "pin_11", "not5_i");
+    circuit->addComponent("output", "pin_10", "not5_o");
 
-    circuit->addComponent("input", "in_13", "not6_i");
-    circuit->addComponent("output", "out_12", "not6_o");
+    circuit->addComponent("input", "pin_13", "not6_i");
+    circuit->addComponent("output", "pin_12", "not6_o");
 
     circuit->addComponent("_not", "not1");
     circuit->addComponent("_not", "not2");
